8c8f3b943dcbd4290fbfc756d2e5562497a08ab4
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-03-07 09:48:58 -0500

    Include style for menu; Make draw tool size larger

--- src/whiteboard.js
+++ src/whiteboard.js
@@ -3,6 +3,7 @@
 console.log('hello');
 
 chrome.runtime.onMessage.addListener(createCanvas);
+let lineWeight = 5;
 
 function createCanvas(message, sender, sendResponse) {
   // grab canvas and create a 2d context
@@ -16,9 +17,12 @@ function createCanvas(message, sender, sendResponse) {
 
     // initialize global variables
     let isMouseDown = false;
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 1;
     let startX;
     let startY;
+
+    // add the menu
+    const menu = createMenu(ctx);
 
     // set the whiteboard to fill the window
     whiteboard.height = window.innerHeight;
@@ -36,7 +40,7 @@ function createCanvas(message, sender, sendResponse) {
 
     function handleDraw(e) {
       if (!isMouseDown) return;
-
+      console.log(ctx.lineWidth);
       // update the position
       startX = e.clientX;
       startY = e.clientY;
@@ -51,6 +55,7 @@ function createCanvas(message, sender, sendResponse) {
     }
 
     document.body.prepend(whiteboard);
+    document.body.append(menu);
     whiteboard.addEventListener('mousedown', handleDrawStart);
     whiteboard.addEventListener('mousemove', handleDraw);
     whiteboard.addEventListener('mouseup', handleDrawStop);
@@ -59,4 +64,28 @@ function createCanvas(message, sender, sendResponse) {
     const canvas = document.getElementById('whiteboard');
     canvas.remove();
   }
+}
+
+function createMenu(ctx) {
+  const menu = document.createElement('div');
+  menu.id = 'menu';
+  const buttons = Array(3)
+    .fill(1)
+    .forEach((el, i) => {
+      const btn = document.createElement('button');
+      btn.dataset.size = (i + 1) * (i + 1);
+      btn.innerText = `${i + 1}`;
+      menu.append(btn);
+    });
+
+  // TODO: dataset is in a string form
+  menu.addEventListener('click', (e) => {
+    const size = e.target.dataset.size;
+
+    if (size) {
+      ctx.lineWidth = +size;
+    }
+  });
+
+  return menu;
 }
